5a5de1553c0a6c9cd2c807e3fcacf85116b21e61
mikm1 <mikm1@comcast.net>
2024-12-19 10:07:35 -0500

    fix vanilla bug allowing infinite casting of BA scrolls

--- SolastaUnfinishedBusiness/Patches/RulesetItemDevicePatcher.cs
+++ SolastaUnfinishedBusiness/Patches/RulesetItemDevicePatcher.cs
@@ -19,6 +19,23 @@ public static class RulesetItemDevicePatcher
             RulesetDeviceFunction function,
             RulesetCharacter character)
         {
+            //PATCH: fix vanilla bug allowing infinite casts of bonus action scrolls
+            var scroll = function.UsableItemDefinition.UsableDeviceDescription.UsableDeviceTags.Contains("Scroll");
+
+            if (scroll && function.DeviceFunctionDescription.SpellDefinition.ActivationTime == ActivationTime.BonusAction)
+            {
+                __result = GameLocationCharacter.GetFromActor(character).GetActionTypeStatus(ActionType.Bonus) != ActionStatus.Spent &&
+                    GameLocationCharacter.GetFromActor(character).GetActionTypeStatus(ActionType.Bonus) != ActionStatus.Unavailable;
+                return;
+            }
+
+            if (scroll && function.DeviceFunctionDescription.SpellDefinition.ActivationTime == ActivationTime.Action)
+            {
+                __result = GameLocationCharacter.GetFromActor(character).GetActionTypeStatus(ActionType.Main) != ActionStatus.Spent &&
+                    GameLocationCharacter.GetFromActor(character).GetActionTypeStatus(ActionType.Main) != ActionStatus.Unavailable;
+                return;
+            }
+
             if (!__result)
             {
                 return;
